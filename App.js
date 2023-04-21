1483fd17a5509ff0bfccd9caf0d8170bea8a7a5c
Sandipan Mahata <Sandipan.m@muvin.in>
2023-04-21 08:56:43 +0530

    added modal in AppPicker

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
 import AppPicker from "./app/components/AppPicker";
+import { useState } from "react";
+
+const categories = [
+	{ label: "Furniture", value: 1 },
+	{ label: "Clothing", value: 2 },
+	{ label: "Electronics", value: 3 },
+];
 
 export default function App() {
+	const [category, setCategory] = useState();
+
 	return (
 		<Screen>
-			<AppPicker iconName="apps" placeholder="Category" />
+			<AppPicker
+				iconName="apps"
+				placeholder="Category"
+				items={categories}
+				selectedItem={category}
+				onSelectItem={(item) => setCategory(item)}
+			/>
 		</Screen>
 	);
 }
--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,31 +1,71 @@
-import { StyleSheet, TextInput, View } from "react-native";
-import React from "react";
+import {
+	FlatList,
+	Modal,
+	StyleSheet,
+	TextInput,
+	TouchableWithoutFeedback,
+	View,
+} from "react-native";
+import React, { useState } from "react";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import defaultStyles from "../config/styles";
 import AppText from "./AppText";
+import Screen from "./Screen";
+import PickerItem from "./PickerItem";
 
-export default function AppPicker({ iconName, placeholder }) {
+export default function AppPicker({
+	iconName,
+	placeholder,
+	items,
+	selectedItem,
+	onSelectItem,
+}) {
+	const [modalVisible, setModalVisible] = useState(false);
+
+	const onPressHandler = (item) => {
+		setModalVisible(false);
+		onSelectItem(item);
+	};
+
 	return (
-		<View style={styles.container}>
-			{iconName && (
-				<MaterialCommunityIcons
-					name={iconName}
-					size={20}
-					color={defaultStyles.colors.medium}
-					style={styles.icon}
-				/>
-			)}
+		<>
+			<TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+				<View style={styles.container}>
+					{iconName && (
+						<MaterialCommunityIcons
+							name={iconName}
+							size={20}
+							color={defaultStyles.colors.medium}
+							style={styles.icon}
+						/>
+					)}
 
-			<AppText style={styles.text}>{placeholder}</AppText>
+					<AppText style={styles.text}>
+						{selectedItem ? selectedItem.label : placeholder}
+					</AppText>
 
-			<MaterialCommunityIcons
-				name="chevron-down"
-				size={20}
-				color={defaultStyles.colors.medium}
-			/>
-		</View>
+					<MaterialCommunityIcons
+						name="chevron-down"
+						size={20}
+						color={defaultStyles.colors.medium}
+					/>
+				</View>
+			</TouchableWithoutFeedback>
+
+			<Modal visible={modalVisible} animationType="slide">
+				<Screen>
+					<FlatList
+						data={items}
+						keyExtractor={(item) => item.label}
+						renderItem={({ item }) => (
+							<PickerItem {...item} onPress={() => onPressHandler(item)} />
+						)}
+					/>
+				</Screen>
+			</Modal>
+		</>
 	);
 }
 
