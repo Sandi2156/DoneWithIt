34257ca54c8135bd98fa20fe285fad8b1119fbab
Sandipan Mahata <Sandipan.m@muvin.in>
2023-04-25 19:28:50 +0530

    created AppNavigator

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import AppText from "./app/components/AppText";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthNavigator from "./app/nagivation/AuthNavigator";
 import navigationTheme from "./app/nagivation/navigationTheme";
+import AppNavigator from "./app/nagivation/AppNavigator";
 
 export default function App() {
 	return (
 		<NavigationContainer theme={navigationTheme}>
-			<AuthNavigator />
+			<AppNavigator />
 		</NavigationContainer>
 	);
 }
