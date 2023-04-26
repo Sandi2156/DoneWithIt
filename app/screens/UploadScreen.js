fcf66554d47c752628d1dbc6990fa7a54781ad5f
Sandipan Mahata <Sandipan.m@muvin.in>
2023-04-26 20:18:03 +0530

    added done animation lottie

--- app/screens/UploadScreen.js
+++ app/screens/UploadScreen.js
@@ -2,14 +2,33 @@ import { StyleSheet, View, Modal } from "react-native";
 import React from "react";
 
 import * as Progress from "react-native-progress";
+import LottieView from "lottie-react-native";
 
 import colors from "../config/colors";
 
-export default function UploadScreen({ progress = 0, visible = false }) {
+export default function UploadScreen({
+	onFinish,
+	progress = 0,
+	visible = false,
+}) {
 	return (
 		<Modal visible={visible}>
 			<View style={styles.container}>
-				<Progress.Bar progress={progress} color={colors.primary} width={200} />
+				{progress < 1 ? (
+					<Progress.Bar
+						progress={progress}
+						color={colors.primary}
+						width={200}
+					/>
+				) : (
+					<LottieView
+						source={require("../assets/animations/done.json")}
+						autoPlay
+						loop={false}
+						onAnimationFinish={onFinish}
+						style={styles.lottie}
+					/>
+				)}
 			</View>
 		</Modal>
 	);
@@ -17,4 +36,5 @@ export default function UploadScreen({ progress = 0, visible = false }) {
 
 const styles = StyleSheet.create({
 	container: { flex: 1, alignItems: "center", justifyContent: "center" },
+	lottie: { width: 150, height: 150 },
 });
